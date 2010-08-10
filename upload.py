7941872b549cd63a65ef114604fff53875804e80
Joseph Turian <turian@gmail.com>
2010-08-10 05:55:21 -0700

    Began fleshing out upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""
+USAGE:
+    ./upload.py file.json
 
-import common.hyperparameters
+NOTE:
+    This doesn't work, never has.
+"""
+
+import sys
+
+import common.hyperparameters, common.options
 HYPERPARAMETERS = common.hyperparameters.read("crowdflower")
+HYPERPARAMETERS, options, args, newkeystr = common.options.reparse(HYPERPARAMETERS)
+
+assert len(args) == 1       # Must pass one filename (JSON file) as arg
+
+file_to_upload = args[0]
+
+print >> sys.stderr, "Uploading file: %s" % file_to_upload
+
+
+import urllib2_file
+import urllib2
+data = { 'form_name':    open(file_to_upload) }
+
+u = urllib2.urlopen('http://api.crowdflower.com/v1/jobs/upload.json?key=%s' % HYPERPARAMETERS["API key"], data)
+print u
+
